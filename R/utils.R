ab385db7aa7122a3443b09cb1eddd1077a711c57
randef1ned <46381867+randef1ned@users.noreply.github.com>
2024-04-07 18:02:24 +0800

    Removed limma package

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,6 @@
 #' @export
 #'
 #' @importFrom checkmate assert_character
-#' @importFrom limma alias2SymbolUsingNCBI
 #' @importFrom utils data download.file
 #' 
 #' @examples
@@ -44,4 +43,68 @@ update_gene_symbol <- function(gene.pool, ncbi = NA, species = c(
   }
   lookup <- alias2SymbolUsingNCBI(gene.pool, ncbi)$Symbol
   return(lookup)
+}
+
+
+##  ALIAS2SYMBOL.R
+## Function comes from https://git.bioconductor.org/packages/limma
+## Modified using fastmatch::fmatch() function
+
+#' Convert Gene Aliases to Official Gene Symbols
+#' 
+#' @description
+#' Maps gene alias names to official gene symbols.
+#' 
+#' @param alias character vector of gene aliases
+#' 
+#' @param gene.info.file the name of a gene information file downloaded from the NCBI.
+#' 
+#' @param required.columns character vector of columns from the gene information file that are required in the output.
+#' 
+#' @return A data frame with the latest gene symbols
+#' 
+#' @export
+#'
+#' @importFrom utils read.delim
+#' @importFrom fastmatch fmatch
+#' 
+#' @examples
+#' library(labyrinth)
+#' \donttest{update_gene_symbol('IL-6')}
+#' 
+alias2SymbolUsingNCBI <- function(alias, 
+                                  gene.info.file, 
+                                  required.columns = c("GeneID", "Symbol", "description")) {
+  #	Convert gene aliases to current symbols etc using a gene_info file downloaded from the NCBI
+  #	Gordon Smyth
+  #	Created 2 March 2017. Last modified 9 June 2020.
+  #	Check input. If necessary, read the gene info file.
+  alias <- as.character(alias)
+  if(is.data.frame(gene.info.file)) {
+    OK <- all(c("GeneID","Symbol","Synonyms") %in% names(gene.info.file))
+    if(!OK) stop("The gene.info.file must include columns GeneID, Symbol and Synonyms")
+    NCBI <- gene.info.file
+    NCBI$Symbol <- as.character(NCBI$Symbol)
+  } else {
+    gene.info.file <- as.character(gene.info.file)
+    NCBI <- read.delim(gene.info.file,comment.char="",quote="",colClasses="character")
+  }
+  
+  #	Try matching to symbols
+  m <- fmatch(alias,NCBI$Symbol)
+  EntrezID <- NCBI$GeneID[m]
+  
+  #	For any rows that don't match symbols, try synonyms
+  i <- which(is.na(EntrezID))
+  if(any(i)) {
+    S <- strsplit(NCBI$Synonyms,split="\\|")
+    N <- vapply(S,length,1)
+    Index <- rep.int(1:nrow(NCBI),times=N)
+    IS <- data.frame(Index=Index,Synonym=unlist(S),stringsAsFactors=FALSE)
+    m <- fmatch(alias[i],IS$Synonym)
+    EntrezID[i] <- NCBI$GeneID[IS$Index[m]]
+    m <- fmatch(EntrezID,NCBI$GeneID)
+  }
+  
+  NCBI[m,required.columns,drop=FALSE]
 }
